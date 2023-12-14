b82ce7a92effa95538955e8201e2992aa34e2669
Abimael Santos <abymaelsantos@gmail.com>
2023-12-14 18:44:13 -0300

    Criação de projeto em Java que Soma de 1 até 1000

---
+++ src/Somador/Somador.java
@@ -0,0 +1,15 @@
+
+package Somador;
+
+public class Somador {
+
+    public static void main(String[] args) {
+        
+        //A soma de 1 até 1000.
+        for (int cont=1; cont<=1000; cont++){
+             System.out.println(cont);
+             cont=cont++;
+        }
+    }
+    
+}
